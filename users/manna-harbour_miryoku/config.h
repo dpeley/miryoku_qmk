39125b7e37e426e1ba28f289bbc138bd41ea2121
dpeley <dpeley@gmail.com>
2025-04-11 15:58:50 -0500

    Update config.h
    Holykeeb and base settings sync

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -8,8 +8,9 @@
 #include "custom_config.h"
 
 // default but used in macros
+//////This number was brought down from 200 for quicker homerow response
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define TAPPING_TERM 190
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -35,8 +36,70 @@
 #define MOUSEKEY_TIME_TO_MAX    64
 
 // Thumb Combos
+//////This number was brought down from 200 for quicker homerow response
 #if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
   #define COMBO_COUNT 8
-  #define COMBO_TERM 200
+  #define COMBO_TERM 190
   #define EXTRA_SHORT_COMBOS
+#endif
+
+/*
+#############################
+# idank config starts below #
+#############################
+*/
+
+#pragma once
+
+// We default to the right hand being the master.
+#undef MASTER_LEFT
+#undef EE_HANDS
+#undef MASTER_RIGHT
+
+
+// Old before shorted TRRS pin. https://docs.holykeebs.com/troubleshooting/#split-side-is-not-working
+#define SERIAL_USART_TX_PIN GP1 //This is the default setting
+//#define SERIAL_USART_TX_PIN GP0 //Comment this out if returning to default
+
+#ifdef TRACKBALL_POSITION_LEFT
+#define MASTER_LEFT
+#else
+#define MASTER_RIGHT
+#endif
+
+
+#ifdef TRACKBALL_POSITION_LEFT
+#define POINTING_DEVICE_ROTATION_270
+#endif
+#ifdef TRACKBALL_POSITION_RIGHT
+#define POINTING_DEVICE_ROTATION_90
+#endif
+#ifdef TRACKBALL_POSITION_THUMB
+#endif
+
+// Common settings to both busywait and interrupt.
+#if defined(PS2_DRIVER_BUSYWAIT) || defined(PS2_DRIVER_INTERRUPT)
+#define PS2_MOUSE_INVERT_X
+#define PS2_MOUSE_INVERT_Y
+
+#define PS2_CLOCK_PIN   D3
+#define PS2_DATA_PIN    B4
+#endif
+
+#ifdef PS2_DRIVER_BUSYWAIT
+#define PS2_MOUSE_USE_REMOTE_MODE
+#endif
+
+#ifdef PS2_DRIVER_INTERRUPT
+#define PS2_INT_INIT()  do {    \
+    EICRA |= ((1<<ISC31) |      \
+              (0<<ISC30));      \
+} while (0)
+#define PS2_INT_ON()  do {      \
+    EIMSK |= (1<<INT3);         \
+} while (0)
+#define PS2_INT_OFF() do {      \
+    EIMSK &= ~(1<<INT3);        \
+} while (0)
+#define PS2_INT_VECT   INT3_vect
 #endif
